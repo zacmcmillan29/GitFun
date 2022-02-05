d30003d89e8662f4b3f5946501215eed8547b3a3
Zac McMillan <84478817+zacmcmillan29@users.noreply.github.com>
2022-02-04 21:13:26 -0700

    program Build_start

---
+++ GitFun/GitFun/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GitFun.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
